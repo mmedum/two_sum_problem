c7b1ffc595ec3d58ff69103e3b9e8ad1fe941681
Mark Medum Bundgaard <mmedum@gmail.com>
2023-11-04 19:10:44 +0400

    Optimizing with searching for the original indicies in the original nums, to answer the problem statement correctly

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,44 @@
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 use std::time::SystemTime;
-use std::collections::HashMap;
-use rand::prelude::*;
-use rand_chacha::ChaCha8Rng;
 
 fn main() {
     let mut rng = ChaCha8Rng::seed_from_u64(1234569);
 
     println!("generating");
     let nums: Vec<i32> = (0..10000).map(|_| rng.gen_range(0..10000)).collect();
-    let search_functions: Vec<(&str, &dyn Fn(Vec<i32>, i32) -> Vec<i32>)> =
-        vec![
+    let search_functions: Vec<(&str, &dyn Fn(Vec<i32>, i32) -> Vec<i32>)> = vec![
         ("sort_stable", &two_sum_sort_stable),
         ("sort_unstable", &two_sum_sort_unstable),
         ("hash", &two_sum_hash),
-        ("naive", &two_sum_naive)
-        ];
-    
+        ("naive", &two_sum_naive),
+    ];
+
     let search_limit_in_secs: u64 = 10;
     for f in &search_functions {
-        let mut run_times : Vec<u128> = Vec::new();
+        let mut run_times: Vec<u128> = Vec::new();
         println!("Function {} start", f.0);
         let start_search = SystemTime::now();
         let mut search_duration: u64 = 0;
         while search_duration < search_limit_in_secs {
             let run_nums = nums.clone();
             let start_iteration = SystemTime::now();
-        
+
             (f.1)(run_nums, -1);
-        
+
             let end = SystemTime::now();
             let iteration_duration = end.duration_since(start_iteration).unwrap().as_nanos();
-            
+
             print!(".");
             io::stdout().flush().unwrap();
             run_times.push(iteration_duration);
-            
+
             search_duration = end.duration_since(start_search).unwrap().as_secs();
         }
-    
+
         let sum = run_times.iter().sum::<u128>();
         let count = run_times.len() as u128;
         let mean = sum / count;
@@ -48,30 +47,52 @@ fn main() {
     }
 }
 
-fn two_sum_sort_stable(mut nums: Vec<i32>, target: i32) -> Vec<i32> {
-    nums.sort();
-    return two_sum_inner_sort(nums, target);
+fn two_sum_sort_stable(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut elements = nums.clone();
+    elements.sort();
+    let targets = two_sum_sort_find_values(elements, target);
+    return two_sum_sort_search_for_indices(nums, targets);
 }
 
-fn two_sum_sort_unstable(mut nums: Vec<i32>, target: i32) -> Vec<i32> {
-    nums.sort_unstable();
-    return two_sum_inner_sort(nums, target);
+fn two_sum_sort_unstable(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut elements = nums.clone();
+    elements.sort_unstable();
+    let targets = two_sum_sort_find_values(elements, target);
+    return two_sum_sort_search_for_indices(nums, targets);
 }
 
-fn two_sum_inner_sort(nums: Vec<i32>, target: i32) -> Vec<i32> {
+fn two_sum_sort_search_for_indices(nums: Vec<i32>, targets: (i32, i32)) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::with_capacity(2);
+    let mut i = 0;
+    while i < nums.len() && result.len() < 2 {
+        let num = nums[1];
+        if targets.0 == num {
+            result.push(num);
+        } else if targets.1 == num {
+            result.push(num);
+        }
+
+        i = i + 1;
+    }
+
+    return result;
+}
+
+fn two_sum_sort_find_values(nums: Vec<i32>, target: i32) -> (i32, i32) {
     let mut i = 0;
     let mut j = nums.len() - 1;
     while i < j {
         let temp = nums[i] + nums[j];
         if temp < target {
             i = i + 1;
-        } else if temp > target{
+        } else if temp > target {
             j = j - 1;
         } else {
-            return vec![i as i32, j as i32];
+            return (nums[i], nums[j]);
         }
     }
-    return Vec::new();
+
+    return (-1, -1);
 }
 
 fn two_sum_hash(nums: Vec<i32>, target: i32) -> Vec<i32> {
@@ -86,8 +107,8 @@ fn two_sum_hash(nums: Vec<i32>, target: i32) -> Vec<i32> {
 }
 
 fn two_sum_naive(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.len()-1 {
-        for j in i+1..nums.len() {
+    for i in 0..nums.len() - 1 {
+        for j in i + 1..nums.len() {
             if nums[i] + nums[j] == target {
                 return vec![i as i32, j as i32];
             }
