a70f3a1cc3257364313e4d538e6b30bf241afff5
Mark Medum Bundgaard <mmedum@gmail.com>
2023-11-19 00:24:44 +0900

    Adding binary search function for sorted solution

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,15 @@ fn main() {
     let mut rng = ChaCha8Rng::seed_from_u64(1234569);
 
     println!("generating...");
-    let nums: Vec<i32> = (0..1000000).map(|_| rng.gen_range(0..1000000)).collect();
+    let nums: Vec<i32> = (0..100000).map(|_| rng.gen_range(0..100000)).collect();
+    let target = -1;
+    //let nums: Vec<i32> = vec![2, 7, 11, 15];
+    //let target = 9;
     let search_functions: Vec<(&str, &dyn Fn(Vec<i32>, i32) -> Vec<i32>)> = vec![
         ("sort_stable_linear", &two_sum_sort_stable_linear_search),
         ("sort_unstable_linear", &two_sum_sort_unstable_linear_search),
+        ("sort_stable_binary", &two_sum_sort_stable_binary_search),
+        ("sort_unstable_binary", &two_sum_sort_unstable_binary_search),
         ("sort_mem", &two_sum_sort_mem_search),
         ("sort_unstable_mem", &two_sum_sort_unstable_mem_search),
         ("hash", &two_sum_hash),
@@ -27,10 +32,11 @@ fn main() {
         io::stdout().flush().unwrap();
         let start_search = SystemTime::now();
         let mut search_duration: Duration = Duration::from_nanos(0);
+        let mut result: Vec<i32> = Vec::new();
         while search_duration.as_secs() < 60 {
             let run_nums = nums.clone();
             let start_iteration = SystemTime::now();
-            (f.1)(run_nums, -1);
+            result = (f.1)(run_nums, target);
             let end = SystemTime::now();
             let iteration_duration = end.duration_since(start_iteration).unwrap().as_nanos();
             run_times.push(iteration_duration);
@@ -47,9 +53,11 @@ fn main() {
         let mean = sum / count;
         println!();
         println!(
-            "  mean runtime {} nanos ({} iterations)",
+            "  mean runtime {} nanos ({} iterations), result {:?} for target {}",
             mean.to_formatted_string(&Locale::da),
-            count.to_formatted_string(&Locale::da)
+            count.to_formatted_string(&Locale::da),
+            result,
+            target
         );
     }
 }
@@ -92,18 +100,18 @@ fn two_sum_sort_mem_find_values(elements: Vec<(i32, i32)>, target: i32) -> Vec<i
 fn two_sum_sort_stable_linear_search(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut elements = nums.clone();
     elements.sort();
-    let targets = two_sum_sort_find_values(elements, target);
+    let targets = two_sum_sort_two_pointer_find_values(elements, target);
     return two_sum_sort_search_for_original_indices(nums, targets);
 }
 
 fn two_sum_sort_unstable_linear_search(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut elements = nums.clone();
     elements.sort_unstable();
-    let targets = two_sum_sort_find_values(elements, target);
+    let targets = two_sum_sort_two_pointer_find_values(elements, target);
     return two_sum_sort_search_for_original_indices(nums, targets);
 }
 
-fn two_sum_sort_find_values(nums: Vec<i32>, target: i32) -> (i32, i32) {
+fn two_sum_sort_two_pointer_find_values(nums: Vec<i32>, target: i32) -> (i32, i32) {
     let mut i = 0;
     let mut j = nums.len() - 1;
     while i < j {
@@ -118,6 +126,55 @@ fn two_sum_sort_find_values(nums: Vec<i32>, target: i32) -> (i32, i32) {
     }
 
     return (-1, -1);
+}
+
+fn two_sum_sort_stable_binary_search(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut elements = nums.clone();
+    elements.sort();
+    let targets = two_sum_sort_binary_search_find_values(elements, target);
+    return two_sum_sort_search_for_original_indices(nums, targets);
+}
+
+fn two_sum_sort_unstable_binary_search(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut elements = nums.clone();
+    elements.sort_unstable();
+    let targets = two_sum_sort_binary_search_find_values(elements, target);
+    return two_sum_sort_search_for_original_indices(nums, targets);
+}
+
+fn two_sum_sort_binary_search_find_values(elements: Vec<i32>, target: i32) -> (i32, i32) {
+    for i in 0..elements.len() {
+        let search_key = target - elements[i];
+        let search_result = two_sum_sort_binary_search(&elements[i..elements.len()], search_key);
+
+        if search_result > 0 {
+            return (elements[i], search_key);
+        }
+    }
+
+    return (-1, -1);
+}
+
+fn two_sum_sort_binary_search(search_elements: &[i32], search_target: i32) -> i32 {
+    let mut low: i32 = 0;
+    let mut high: i32 = search_elements.len() as i32 - 1;
+
+    while low <= high {
+        let mid = low + (high - low) / 2;
+        let element = search_elements[mid as usize];
+
+        if element == search_target {
+            return element;
+        }
+
+        if element < search_target {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
 }
 
 fn two_sum_sort_search_for_original_indices(nums: Vec<i32>, targets: (i32, i32)) -> Vec<i32> {
